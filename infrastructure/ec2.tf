8f41edd2749cb184e7461914818225adaff378d2
Ozarkiv Ihor <ozarkiv.ihor@gmail.com>
2018-11-01 08:17:50 +0200

    prod_keystone.js

---
+++ infrastructure/ec2.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  access_key = "AKIAIKNHXZ54LDE6AGIA"
+  secret_key = "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"
+  region     = "us-east-2"
+}
+#1
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+#2
+resource "aws_instance" "example" {
+         ami = "ami-0e71f566510325d43"
+         vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+         instance_type = "t2.micro"
+         key_name = "deploy_ansible"
+
+         tags {
+         Name = "terraform-master"
+         }
+}
+resource "aws_instance" "example1" {
+         ami = "ami-0e71f566510325d43"
+         vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+         instance_type = "t2.micro"
+         key_name = "deploy_ansible"
+
+         tags {
+         Name = "terraform-web"
+         }
+}
+resource "aws_instance" "example2" {
+         ami = "ami-0e71f566510325d43"
+         vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+         instance_type = "t2.micro"
+         key_name = "deploy_ansible"
+
+         tags {
+         Name = "terraform-mongodb"
+         }
+}
